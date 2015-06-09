aa65b7afd5b587fab8fafc0fa62823b7cd5b1a68
Dinozor <DinoCrys@gmail.com>
2015-06-09 19:49:29 +0300

    Initial commit

---
+++ ReadWriteExample_C.c
@@ -0,0 +1,10 @@
+// ReadWriteExample_C.cpp : Defines the entry point for the console application.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	system("pause");
+	return 0;
+}
